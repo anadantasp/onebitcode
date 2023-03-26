071078ad9f8d825627520dba0d6569535538f534
anadantasp <caroldantasp@gmail.com>
2023-03-26 15:44:39 -0300

    exercicio react 01 - Lorem Ipsum

---
+++ react/exercicio-react-lorem-ipsum/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoremIpsum from './components/LoremIpsum';
+import HelloWorld from './components/Hello';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld/>
+      <LoremIpsum/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/exercicio-react-lorem-ipsum/src/components/LoremIpsum.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoremIpsum = () =>{
+    return <p>
+            Dolore exercitation exercitation aute reprehenderit est. Excepteur enim nisi minim 
+            reprehenderit commodo. Amet proident ea id voluptate ea in laboris veniam sit nostrud 
+            amet nisi occaecat duis. Eu velit magna do proident Lorem. Est reprehenderit excepteur 
+            consectetur ipsum ex tempor in proident aliqua aliqua non esse excepteur. Esse anim laborum 
+            irure aliqua enim magna duis consectetur ea incididunt nostrud ut elit eiusmod.
+        </p>
+}
+
+export default LoremIpsum;
